5462d6079c34302af8a3b27e56f8eb9fdbbd0b8d
unknown <akibrahim@WSRSDEV-01.mbn-news.com>
2014-09-02 10:18:57 -0400

    Tailored the code for Wordpress and added it all in one folder called VizLib. This is the library that would be included within the Wordpress install's directory structure

---
+++ VizLib/Article.php
@@ -0,0 +1,41 @@
+<?php
+	/**
+		This class is responsible for registering the metadata pertinent to an article
+	*/
+	class Article {
+		private $db = "";
+		
+		/**
+			assign database instance in constructor
+		*/
+		public function __construct($db,$metadata = Array()) {
+			$this->db=$db;
+			$this->register($metadata);
+		}
+		
+		/**
+			registers the article's title,category,url,sample text, and a sample image
+		*/
+		private function register($metadata) {
+			// get article id
+			$article_id = $metadata["article_id"];
+			
+			// insert the article metadata into the database
+			$category_id = $metadata["category_id"];
+			$article_url = $metadata["url"];
+			$title = $metadata["title"];
+			$sample_text = $metadata["sample_text"];
+			$sample_pic = $metadata["sample_pic"];
+			$sql = "insert into article(article_id,category_id,article_url,title,sample_text,sample_pic) values (:article_id,:category_id,:article_url,:title,:sample_text,:sample_pic)";
+			$sth = $this->db->prepare($sql);
+
+			$sth->bindParam(":article_id",$article_id,PDO::PARAM_INT);
+			$sth->bindParam(":category_id",$category_id,PDO::PARAM_INT);
+			$sth->bindParam(":article_url",$article_url,PDO::PARAM_STR);
+			$sth->bindParam(":title",$title,PDO::PARAM_STR);
+			$sth->bindParam(":sample_text",$sample_text,PDO::PARAM_STR);
+			$sth->bindParam(":sample_pic",$sample_pic,PDO::PARAM_STR);
+			$sth->execute();
+		}
+	}
+?>
